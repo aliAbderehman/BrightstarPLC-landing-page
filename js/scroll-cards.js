e91010491fa75d83ea28510346c048c19e25257d
Abderehman <ali.abderehman4@gmail.com>
2025-05-20 17:21:20 +0300

    changed the look of services secion

--- js/scroll-cards.js
+++ js/scroll-cards.js
@@ -1,7 +1,7 @@
 const container = document.getElementById("cardContainer");
 const scrollAmount = 300; // Adjust based on your card width
 
-const cardWidth = container.querySelector(".blog__card").offsetWidth + 16; // includes gap
+const cardWidth = container.querySelector(".blog__card").offsetWidth + 45; // includes gap
 
 function scrollNext() {
   // container.scrollBy({ left: scrollAmount, behavior: "smooth" });
